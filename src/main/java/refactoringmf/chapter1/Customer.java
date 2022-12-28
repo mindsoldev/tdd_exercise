19c2ebc5e33134e30cf1bdfae548f92c4ec18ee1
Harsan Peter <pharsan@yahoo.com>
2022-12-28 16:13:42 +0100

    Extract getFrequentRenterPoints and move to Rental

--- src/main/java/refactoringmf/chapter1/Customer.java
+++ src/main/java/refactoringmf/chapter1/Customer.java
@@ -27,11 +27,7 @@ public class Customer {
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
 
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += rental.getFrequentRenterPoints();
 
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
--- src/main/java/refactoringmf/chapter1/Rental.java
+++ src/main/java/refactoringmf/chapter1/Rental.java
@@ -41,4 +41,11 @@ public class Rental {
 		return result;
 	}
 
+	int getFrequentRenterPoints() {
+		if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) 
+		    return 2;
+		else 
+			return 1;
+	}
+
 }
