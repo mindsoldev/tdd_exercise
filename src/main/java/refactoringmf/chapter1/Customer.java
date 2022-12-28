242d9bd143d253cfff4f1878f407e67e38fe32ac
Harsan Peter <pharsan@yahoo.com>
2022-12-28 16:01:51 +0100

    Extract switch  -> getCharge and move to Rental

--- src/main/java/refactoringmf/chapter1/Customer.java
+++ src/main/java/refactoringmf/chapter1/Customer.java
@@ -26,22 +26,6 @@ public class Customer {
 
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-            double amount = 0;
-            switch (rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    amount += 2;
-                    if (rental.getDaysRented() > 2)
-                        amount += (rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    amount += rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    amount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        amount += (rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -50,9 +34,9 @@ public class Customer {
                 frequentRenterPoints++;
 
             // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
 
-            totalAmount += amount;
+            totalAmount += rental.getCharge();
         }
 
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
@@ -60,4 +44,5 @@ public class Customer {
 
         return result;
     }
+
 }
